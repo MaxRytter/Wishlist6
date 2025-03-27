53bb1f6cd6fb306940291c00a8ff2e6fd27b1302
MaxRytter <maxrytter97@hotmail.com>
2025-03-27 15:28:24 +0100

    igenigen

--- src/test/java/org/example/wishlist6/Wishlist6ApplicationTests.java
+++ src/test/java/org/example/wishlist6/Wishlist6ApplicationTests.java
@@ -1,16 +1,21 @@
-package org.example.wishlist6;
+    package org.example.wishlist6;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+    import org.springframework.context.ApplicationContext;
+    import org.junit.jupiter.api.Test;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+    import static org.junit.jupiter.api.Assertions.assertNotNull;
+    import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-class Wishlist6ApplicationTests {
+    @SpringBootTest(classes = Wishlist6Application.class)
+    class Wishlist6ApplicationTests {
+        @Autowired
+        private ApplicationContext applicationContext;
+        @Test
+        void contextLoads() {
+            assertNotNull(applicationContext);
+        }
 
-    @Test
-    void contextLoads() {
-        assertTrue(true);
+
     }
-
-}

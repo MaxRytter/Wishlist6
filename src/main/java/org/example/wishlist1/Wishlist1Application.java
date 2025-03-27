afd3b9709529e70855bcbe54f8919840873ca8d4
MaxRytter <maxrytter97@hotmail.com>
2025-03-27 13:41:08 +0100

    Initial commit

---
+++ src/main/java/org/example/wishlist1/Wishlist1Application.java
@@ -0,0 +1,13 @@
+package org.example.wishlist1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wishlist1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wishlist1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wishlist1/Wishlist1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wishlist1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wishlist1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

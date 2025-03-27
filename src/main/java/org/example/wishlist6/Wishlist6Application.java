5c6507049109c542dd3d3ae8d48b693400a9c794
MaxRytter <maxrytter97@hotmail.com>
2025-03-27 14:39:41 +0100

    renameigen

--- src/main/java/org/example/wishlist6/Wishlist6Application.java
+++ src/main/java/org/example/wishlist6/Wishlist6Application.java
@@ -1,13 +1,13 @@
-package org.example.wishlist1;
+package org.example.wishlist6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Wishlist1Application {
+public class Wishlist6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Wishlist1Application.class, args);
+        SpringApplication.run(Wishlist6Application.class, args);
     }
 
 }
--- src/test/java/org/example/wishlist6/Wishlist6ApplicationTests.java
+++ src/test/java/org/example/wishlist6/Wishlist6ApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.wishlist1;
+package org.example.wishlist6;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class Wishlist1ApplicationTests {
+class Wishlist6ApplicationTests {
 
     @Test
     void contextLoads() {

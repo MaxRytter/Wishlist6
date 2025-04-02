f79cad864c2038ea104809d4f06cca7e5d8147df
mcmadsv <madsdamv@outlook.com>
2025-04-02 13:12:05 +0200

    222222

--- src/main/java/org/example/wishlist6/Repository/WishListRepository.java
+++ src/main/java/org/example/wishlist6/Repository/WishListRepository.java
@@ -5,11 +5,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class WishlistRepository {
+public class WishListRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public WishlistRepository(JdbcTemplate jdbcTemplate) {
+    public WishListRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
--- src/main/java/org/example/wishlist6/Service/WishListService.java
+++ src/main/java/org/example/wishlist6/Service/WishListService.java
@@ -1,14 +1,14 @@
 package org.example.wishlist6.Service;
 
 import org.example.wishlist6.Module.Wishlist;
-import org.example.wishlist6.Repository.WishlistRepository;
+import org.example.wishlist6.Repository.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @org.springframework.stereotype.Service
  public class WishListService {
 
     @Autowired
-    public WishlistRepository wishlistRepository;
+    public WishListRepository wishlistRepository;
 
     public void addWishlist(Wishlist wishlist) {
         wishlistRepository.save(wishlist);

0de1ba4dde57108009053ad55db66ec34737a56a
luceedot <asqv0001@stud.kea.dk>
2025-04-08 14:35:06 +0200

    homepage til bruger der er logget ind + log out funktion

--- src/main/java/org/example/wishlist6/Controller/LoginController.java
+++ src/main/java/org/example/wishlist6/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package org.example.wishlist6.Controller;
 
+import org.example.wishlist6.Module.User;
 import org.example.wishlist6.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
+
 
 
 @Controller
@@ -22,14 +25,40 @@ public class LoginController {
 }
 
 @PostMapping("/login")
-    public String loginUser(@RequestParam String userEmail,@RequestParam String userPassword, Model model) {
+    public String loginUser(@RequestParam String userEmail,@RequestParam String userPassword, Model model, HttpSession session) {
+
+    System.out.println("User Email: " + userEmail);
+    System.out.println("User Password: " + userPassword); //slet prints senere, bruger dem bare til debugging
+
     boolean isAuthenticated = userService.authenticateUser(userEmail, userPassword);
     if (isAuthenticated) {
+        session.setAttribute("userEmail", userEmail);
         return "redirect:/home";
-    } else
-    {
-        model.addAttribute("Fejl", "Brugeren kunne ikke genkendes, prøv igen");
+    } else {
+        model.addAttribute("errorMessage", "Brugeren kunne ikke genkendes, prøv igen");
         return "login";
     }
+}
+
+@GetMapping("/home")
+    public String showHome(HttpSession session, Model model) {
+    String userEmail = (String) session.getAttribute("userEmail");
+    if (userEmail == null){
+        return "redirect:/login";
+    }
+
+    User user = userService.getUserByEmail(userEmail);
+    if (user != null) {
+        model.addAttribute("userEmail", userEmail);
+        model.addAttribute("userName", user.getUserName());
+    }
+    return "home";
+}
+
+
+@GetMapping("/logout")
+    public String logout(HttpSession session) {
+    session.invalidate();
+    return "redirect:/login";
 }
 }
